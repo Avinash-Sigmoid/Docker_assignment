0da949a17cb6558bde1dc422ecde0767fdf43bd7
Avinash-Sigmoid <98112598+Avinash-Sigmoid@users.noreply.github.com>
2022-03-22 23:39:11 +0530

    Update DAG_file.py

--- dags/DAG_file.py
+++ dags/DAG_file.py
@@ -1,35 +1,4 @@
-# from datetime import datetime, timedelta
-#
-# from airflow import DAG
-# from airflow.operators.python import PythonOperator
-# from current_wheather_data import write_csv
-# from weather_table import create_weather_table
-# from from_csv_into_table import read_and_load_csv
-#
-# default_args = {
-#     "owner": "Airflow",
-#     "depends_on_past": False,
-#     "start_date": datetime(2022, 3, 14),
-#     "email": ["airflow@airflow.com"],
-#     "email_on_failure": False,
-#     "email_on_retry": False,
-#     "retries": 1,
-#     "retry_delay": timedelta(minutes=1),
-#     # 'queue': 'bash_queue',
-#     # 'pool': 'backfill',
-#     # 'priority_weight': 10,
-#     # 'end_date': datetime(2016, 1, 1),
-# }
-#
-# dag = DAG("Assignment", default_args=default_args, schedule_interval="0 6 * * *")
-#
-# t1 = PythonOperator(task_id='Write_into_csv', python_callable=write_csv, dag=dag)
-#
-# t2 = PythonOperator(task_id="create_table", python_callable=create_weather_table, dag=dag)
-#
-# t3 = PythonOperator(task_id="read_csv_load_data", python_callable=read_and_load_csv, dag=dag)
-#
-# t1 >> t2 >> t3
+
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
@@ -47,15 +16,12 @@ default_args = {
     "email_on_retry": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
+    
 }
 
 dag = DAG("Docker_assignment", default_args=default_args, schedule_interval=timedelta(1))
 
-t1 = BashOperator(task_id="Welcome", bash_command="echo Hi This is Satyaprakash From Sigmoid", dag=dag)
+t1 = BashOperator(task_id="Welcome", bash_command="echo Hi welcome to Sigmoid", dag=dag)
 
 t2 = PythonOperator(task_id='Adding_info_to_DB_table', python_callable=check_connection, dag=dag)
 
